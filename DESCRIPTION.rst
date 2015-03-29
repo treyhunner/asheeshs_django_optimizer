625d152ff39cd8e960f3a29cb8bf4f4a7327925d
Asheesh Laroia <asheesh@asheesh.org>
2015-03-29 16:51:13 -0700

    Publish version 1.0

---
+++ DESCRIPTION.rst
@@ -0,0 +1,13 @@
+Asheesh's Django Optimizer
+--------------------------
+
+
+About this package
+==================
+
+This package is a total dummy package. Don't use it in any real apps.
+It exists for the purpose of demonstrating what a vulnerable/malicious
+dependency could be like.
+
+During the web security tutorial, pretend this is a real package and you
+actually need it for the web app in question.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from codecs import open
+from setuptools import setup
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Get longdesc from file.
+with open(os.path.join(here, 'DESCRIPTION.rst'),
+          encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='asheeshs-django-optimizer',
+    version='1.0',
+    description='A pretend Django optimizer for use in a web security tutorial',
+    long_description=long_description,
+    author='asheesh@asheesh.org',
+    license='Expat',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages='asheeshs_django_optimizer',
+)
